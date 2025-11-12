80d0cd47c1525dda07beffec96eda70f4b7dcd87
Alan Muir <a.muir@ucl.ac.uk>
2025-11-12 16:58:26 +0000

    annual_dh_ql_stage_wrapper.sh

--- annual_dh.php
+++ annual_dh.php
@@ -177,10 +177,10 @@ one year to provide the change of height up until the end of that year.</p>
       <select class="mmv-speed" data-role="speed" aria-label="Playback speed">
         <option value="0.5">0.5×</option>
         <option value="0.75">0.75×</option>
-        <option value="1" selected>1× speed</option>
+        <option value="1" selected>1×speed</option>
         <option value="1.25">1.25×</option>
         <option value="1.5">1.5×</option>
-        <option value="2">2×</option>
+        <option value="2">2×speed</option>
       </select>
     </div>
 
