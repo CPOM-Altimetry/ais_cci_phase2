615a0f01ddab5ea0c479ee571cb5053dc81d1d9b
Alan Muir <a.muir@ucl.ac.uk>
2025-11-20 10:55:29 +0000

    adding back View

--- annual_dh.php
+++ annual_dh.php
@@ -180,7 +180,7 @@ Each frame of the visualization below contains a plot the Cumulative Annual dH f
 }
 
 .adhv-view-select {
-  background:lightgray; color:#fff; border-radius:9px;padding: 4px;
+  background:lightgray; color:#000; border-radius:6px;padding: 6px;
 }
 
 </style>
